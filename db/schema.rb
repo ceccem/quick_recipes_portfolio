1a6dd1327bd84afc5ca00ac4b991f3cf10a0fd13
ceccem <117820613+ceccem@users.noreply.github.com>
2023-11-11 01:57:42 +0900

    usernameカラム追加,deviceビューの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_07_093117) do
+ActiveRecord::Schema.define(version: 2023_11_10_163545) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2023_11_07_093117) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
