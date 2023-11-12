11b2d8e2d926e4513b7073506bb990656b98d1e7
ceccem <117820613+ceccem@users.noreply.github.com>
2023-11-13 04:19:15 +0900

    recipes/showビューの修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
-  process resize_to_limit: [300, 300]
+  process resize_to_limit: [700, 700]
+
+  version :thumb do
+    process resize_to_fill: [200, 200]
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
