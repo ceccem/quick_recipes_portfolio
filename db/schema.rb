77310a2e8e38bb5cafb924a46920ae54aeecadb2
ceccem <117820613+ceccem@users.noreply.github.com>
2023-11-22 10:36:58 +0900

    Deviseにアイコン及びひとことカラム追加・ビューの修正, ユーザーアイコンのリサイズ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_20_004444) do
+ActiveRecord::Schema.define(version: 2023_11_22_012010) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2023_11_20_004444) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "icon"
+    t.text "message"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
