b42ccd4fcba87eb31fd1cdfb48f00473db616f9d
ceccem <117820613+ceccem@users.noreply.github.com>
2024-01-04 20:18:19 +0900

    投稿フォームCSS修正

--- app/models/seasoning.rb
+++ app/models/seasoning.rb
@@ -1,4 +1,4 @@
 class Seasoning < ApplicationRecord
-  has_many :recipe_seasonings
-  has_many :recipes, through: :recipe_seasonings
+  has_many :recipe_seasonings, dependent: :destroy
+  has_many :recipes, through: :recipe_seasonings, dependent: :destroy
 end
